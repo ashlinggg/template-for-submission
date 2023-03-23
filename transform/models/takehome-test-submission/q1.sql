4fbffd03243ca71de6c5113bdbd02a12392543b3
ashlinggg <g.angelinalim@gmail.com>
2023-03-23 23:16:21 +0800

    Modify Q1

--- transform/models/takehome-test-submission/q1.sql
+++ transform/models/takehome-test-submission/q1.sql
@@ -5,16 +5,16 @@
 }}
 
 
-WITH q1 AS (
+WITH total_gmv_by_country AS (
     
     SELECT
-      country_name, 
+        country_name, 
       
-      ROUND(SUM(gmv_local),2) AS total_gmv 
+        ROUND(SUM(gmv_local),2) AS total_gmv 
 
     FROM `majestic-vault-381405`.Orders_Dataset.Orders_Table
 
     GROUP BY country_name
 )
 
-SELECT * FROM q1
+SELECT * FROM total_gmv_by_country
